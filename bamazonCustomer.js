e7325ec42f98766e3ea959e24447839231b3f4dc
buchananc <candacebuchanan7@gmail.com>
2018-06-08 19:21:59 -0400

    saving to reinstall node

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 var chalk = require('chalk');
 var Table = require('cli-table');
-// var figlet = require('figlet');
+var figlet = require('figlet');
 
 //MySQL Database created called bamazon.
 var connection = mysql.createConnection({
@@ -15,26 +15,38 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    begin();
-});
+connection.connect();
 
-///////////////////////////////TRIGGER WELCOME//////////////////////////////////
 
-function begin() {
-    // clear();
-    // console.log(figlet.textSync('BAMAZON', {
-    //     font: 'Ghost',
-    //     horizontalLayout: 'default',
-    //     verticalLayout: 'default'
-    // }));
+///////////////////////////////START OF BAMAZON HEADER//////////////////////////////////
+
+function bamazonReady() {
+    figlet.text('BAMAZON', {
+        font: 'Doom',
+        horizontalLayout: 'default',
+        verticalLayout: 'default'
+    }, function (err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data);
+    });
+    //menu();
+}
+///////////////////////////////END OF BAMAZON HEADER//////////////////////////////////
+
+///////////////////////////////START OF MENU//////////////////////////////////
+// start function to allow customer to purchase a product from item list
+
+function menu() {
 
     inquirer.prompt([{
             name: "action",
             type: "list",
-            message: "Welcome to Bamazon! What would you like to do?",
-            choices: ["View available products", "Exit"]
+            message: "WELCOME TO BAMAZON! WHAT WOULD YOU LIKE TO DO?",
+            choices: ["View available products", "View low inventory", "Add to inventory", "Add a new product", "Exit"]
         }])
         .then(function (answer) {
             switch (answer.action) {
@@ -43,46 +55,97 @@ function begin() {
                     break;
 
                 case "Exit":
-                    connection.end();
+                    exitProgram();
                     break;
             }
         });
 }
-///////////////////////////////END OF WELCOME//////////////////////////////////
+////////////////////////////////////END OF MENU////////////////////////////////////
 
-//////////////////////DISPLAYS INVENTORY IN A TABLE////////////////////////
+//////////////////////DISPLAYS TABLE OF ALL ITEMS FOR SALE////////////////////////
+// The app should then prompt users with two messages.
+// The first will ask the ID of the product they would like to buy.
+// The second message will ask how many units of the product they would like to buy.
+// Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+// If not, the app should log a phrase like Insufficient quantity! , and then prevent the order from going through.
+// However, if your store does have enough of the product, you should fulfill the customer's order.
+
 var seeInventory = function () {
-    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products WHERE stock_quantity > 0", function (err, res, fields) {
+    connection.query("SELECT item_id, product_name, price, department_name, stock_quantity FROM products WHERE stock_quantity > 0", function (err, res, fields) {
         var table = new Table({
-            head: ['ID', 'Product Name', 'Price', 'Quantity']
+            head: ['ID', 'Product Name', 'Price', 'Department', 'Quantity']
         });
-        console.log("These are the remaining items on sale: ");
+        console.log("THESE ARE THE REMAINING ITEMS FOR SALE: ");
         console.log("===========================================================");
         for (var i = 0; i < res.length; i++) {
-            table.push([res[i].item_id, res[i].product_name, res[i].price, res[i].stock_quantity]);
+            table.push([res[i].item_id, res[i].product_name, res[i].price, res[i].department_name, res[i].stock_quantity]);
         }
         console.log(table.toString());
-        console.log("----------------------------------------------------------");  
-        whatId();
+        console.log("----------------------------------------------------------");
+
+        inquirer.prompt([{
+                name: "item_id",
+                type: "input",
+                message: "Enter the ID of the item you wish to purchase: "
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many of this item would you like to purchase: "
+            }
+        ]).then(function (answers) {
+            connection.query("SELECT * FROM products WHERE item_id - " + answers.item_id, function (error, results) {
+                try {
+                    let currentPrice = results[0].price;
+                    var total = (answers.quantity * currentPrice).toFixed(2);
+
+                    if (results[0].stock_quantity < answers.quantity) {
+                        console.log("\n\n\n\n\n");
+                        console.log("Insufficient quantity!");
+                        seeInventory();
+                    } else {
+                        connection.query("UPDATE products SET stock_quantity - " + answers.quantity + "WHERE item_id - " + answers.item_id, function (error, results) {
+                            console.log("Inventory Update!");
+                            console.log("Your total is: $" + total);
+                            console.log("Thank you for shopping at Bamazon!");
+                        });
+                        connection.query("UPDATE departmentsINNER JOIN products ON products.department_name - department.department_name SET departments.product_sales - department.product_sales + ? WHERE products.item_id", function (error, results) {
+                            console.log("Department updated!");
+                            exitProgram();
+                        });
+                    }
+                } catch (e) {
+                    console.log("There was an error with your Bamazon request: " + e.message);
+                    exitProgram();
+                }
+            });
+        });
     });
 };
-///////////////////////////////END OF TABLE FUNCTION//////////////////////////////////
-var whatId = function(){
-    var buySomething = function (answers) {
-        console.log("You chose: ", answers);
-    };
 
-    var questions = [{
-        name: "id",
-        type: "input",
-        message: "Enter the ID of the item you wish to purchase: "
-    },
-    {
-        name: "amount",
-        type: "input",
-        message: "How many of this item would you like to purchase: "
-    }];
-    
-    inquirer.prompt(questions, buySomething);
-};
+function exitProgram() {
+    connection.end();
+}
+///////////////////////////////END OF ITEMS DISPLAYED/////////////////////////////////
 
+
+///////////////////////////////VIEW LOW INVENTORY////////////////////////////////////
+// function lowInventory() {
+//     connection.query("SELECT products WHERE stock_quantity < 5", function(error, results) {
+//         var table = new Table({
+//             head: ['ID', 'Product Name', 'Price', 'Department', 'Quantity']
+//         });
+//         console.log("THESE ARE THE REMAINING ITEMS FOR SALE THAT ARE LOW INVENTORY: ");
+//         console.log("===========================================================");
+//         for(var i = 0; i<res.length; i++) {
+//         table.push([res[i].item_id, res[i].product_name, res[i].price, res[i].department_name, res[i].stock_quantity]);
+//     }
+//     console.log(table.toString());
+//     console.log("----------------------------------------------------------");
+//     menu();
+//     });
+// }
+///////////////////////////////END OF LOW INVENTORY////////////////////////////////////
+
+// This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
